3376189837aee4ef7a94fd722da1df0fab2a066e
fwg <adf0001@163.com>
2022-02-20 16:21:40 +0800

    update: use tpsvr

--- test/test-data.js
+++ test/test-data.js
@@ -1,11 +1,12 @@
-// global, for html page
-ele = require("../ele-tool.js");
+
+//global variable, for html page, refer tpsvr @ npm.
+ele_tool = require("../ele-tool.js");
 
 module.exports = {
 
-	"ele()": function (done) {
-		return ele('divResult') === document.getElementById('divResult') &&
-			ele(document.getElementById('divResult')) === document.getElementById('divResult');
+	"ele_tool()": function (done) {
+		return ele_tool('divResult') === document.getElementById('divResult') &&
+			ele_tool(document.getElementById('divResult')) === document.getElementById('divResult');
 	},
 
 };
@@ -15,4 +16,4 @@ module.exports = {
 if (typeof showResult !== "function") showResult = function (text) { console.log(text); }
 
 //for mocha
-if (typeof describe === "function") describe('mocha-test', function () { for (var i in module.exports) { it(i, module.exports[i]); } });
+if (typeof describe === "function") describe('ele_tool', function () { for (var i in module.exports) { it(i, module.exports[i]).timeout(5000); } });
